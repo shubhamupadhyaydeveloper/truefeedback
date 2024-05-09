1301b26af8929bd2606f5c5cef8438c88b91cc10
ShuhbamUpadhyayDeveloper <shubhamwork48@gmail.com>
2024-05-09 23:49:22 +0530

    route page complete

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,21 +1,121 @@
-'use client'
+"use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import React, { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm, FieldValues } from "react-hook-form";
+import { signinSchema } from "@/schemas/signinSchema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { signIn } from "next-auth/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+const Signin = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: FieldValues) => {
+    try {
+      const response = await signIn("credential", {
+        redirect: false,
+        email: data.email,
+        password: data.password,
+      });
+      console.log("this is response", response);
+      if(response?.error) {
+        toast.error('envalid email or password');
+      }
+
+      if(response?.url) {
+        router.push('/')
+      }
+    } catch {
+      toast.error('envalid email or password');
+    } finally {
+      form.reset()
+    }
+  };
+
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <div className="flex justify-center items-center h-screen">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Welcome back ,we are glad to see you again
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="jondeo@email.com"
+                          {...field}
+                          type="email"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl className="flex justify-between px-4">
+                        <Input
+                          type="password"
+                          placeholder="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
     </>
-  )
-}
+  );
+};
+
+export default Signin;
